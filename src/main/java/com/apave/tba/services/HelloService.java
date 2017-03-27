71dec0d4ccdfd8ab8c4017a69b89c9551cb94798
Thomas Bonavia <thomas.bonavia@apave.com>
2017-03-27 15:29:04 +0200

    Ajout de tests unitaires

--- src/main/java/com/apave/tba/services/HelloService.java
+++ src/main/java/com/apave/tba/services/HelloService.java
@@ -3,6 +3,7 @@ package com.apave.tba.services;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by A4811584 on 27/03/2017.
@@ -12,8 +13,11 @@ public class HelloService {
 
     @GET
     @Path("/{name}")
-    public String hello(@PathParam("name") String name){
-        return "Hello " + name + " !";
+    public Response hello(@PathParam("name") String name) {
+        if(name == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        return Response.ok("Hello " + name + " !").build();
     }
 
 }
---
+++ src/test/java/com/apave/tba/services/HelloServiceTest.java
@@ -0,0 +1,54 @@
+package com.apave.tba.services;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by A4811584 on 27/03/2017.
+ */
+public class HelloServiceTest {
+    private static final String BASE_URI = "http://localhost:9998";//
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+        ResourceConfig config = new ResourceConfig(HelloService.class);
+
+        server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+
+        Client client = ClientBuilder.newClient();
+        target = client.target(BASE_URI);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.shutdown();
+    }
+
+    @Test
+    public void testHelloWithName() {
+        assertEquals("Hello Tom !", target.path("hello").path("Tom").request().get().readEntity(String.class));
+        assertEquals("Hello Jay !", target.path("hello").path("Jay").request().get().readEntity(String.class));
+    }
+
+    @Test
+    public void testHelloWithNoName() {
+        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), target.path("hello").path("").request().get().getStatus());
+    }
+}
+
